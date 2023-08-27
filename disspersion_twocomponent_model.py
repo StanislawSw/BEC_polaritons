220604859d2ffe031063a3129515a79ffdc1b8ec
StanislawSw <80028704+StanislawSw@users.noreply.github.com>
2023-08-27 16:04:54 +0200

    Update and rename disspersion_twocomponent_model to disspersion_twocomponent_model.py

--- disspersion_twocomponent_model.py
+++ disspersion_twocomponent_model.py
@@ -11,6 +11,7 @@ from matplotlib.colors import ListedColormap, BoundaryNorm
 from matplotlib import cm
 from matplotlib import ticker
 from matplotlib import colors
+from tqdm import tqdm
 import csv
 
 font = {'family' : 'serfi','weight' : 'normal','size': 10}
@@ -18,13 +19,12 @@ plt.rc('font', **font)
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
 
-### parametry symulacji
+# SIMULATION PARAMETERS
 # -----------------------------------------------------------------------------#
-Lx=20; Ly=20; n=8; Nx=2**n; Ny=2**n; dt=0.0001; tmax=2; t0 = 0.1;
-#Lx=80; Ly=80; n=8; Nx=2**n; Ny=2**n; dt=0.001; tmax=40; t0 = 0.1;      #Lx=20; Ly=20; n=8; Nx=2**n; Ny=2**n; dt=0.0001; tmax=2; t0 = 0.1;  #Lx=80; Ly=80; n=8; Nx=2**n; Ny=2**n; dt=0.001; tmax=40; t0 = 0.1;#Lx=80; Ly=80; n=8; Nx=2**n; Ny=2**n; dt=0.001; tmax=40; t0 = 0.1;  #Lx=80; Ly=80; n=8; Nx=2**n; Ny=2**n; dt=0.001; tmax=40; t0 = 0.1;      #Lx=20; Ly=20; n=8; Nx=2**n; Ny=2**n; dt=0.0001; tmax=2; t0 = 0.1;  #Lx=80; Ly=80; n=8; Nx=2**n; Ny=2**n; dt=0.001; tmax=40; t0 = 0.1;
-dx=Lx/Nx; dy=Ly/Ny; nmax=round((tmax-t0)/dt)
+Lx=20; Ly=20; n=8; Nx=2**n; Ny=2**n; dt=0.0001; tmax=2; t0 = 0.1; 
+Lx=80; Ly=80; n=8; Nx=2**n; Ny=2**n; dt=0.001; tmax=40; t0 = 0.1;    
 # -----------------------------------------------------------------------------#
-# Physical parameters----------------------------------------------------------#
+# PHYSICAL PARAMETERS
 # -----------------------------------------------------------------------------#
 hbar    =  0.65821195;              # Dirac constant                  [meV ps]
 E_0     =  510998946.1;             # Free electron energy               [meV]
@@ -33,31 +33,38 @@ m_e0    =  E_0/(c**2);              # Free electron mass    [(meV ps^2)/mum^2]
 n_cav   =  0.65#0.65;               # Refraction index of microcavites     [-]
 # -----------------------------------------------------------------------------#
 Delta   = 0                         # Detuning                           [meV]
-E_x0  = 0;                     # Exciton resonance enrgy            [meV]
-E_c0  = -2;                     # Photon resonance energy            [meV]
+E_x0  = 0;                          # Exciton resonance enrgy            [meV]
+E_c0  = -2;                         # Photon resonance energy            [meV]
 OmegaR  = 9.8/hbar                  # Rabi splitting
 # -----------------------------------------------------------------------------#
 m_cav   = 0.5*(10**(-4))*m_e0;      # Photon effective mass  [(meV ps^2)/mum^2]
 m_exc   = 0.1*m_e0;                 # Exciton effective mass [(meV ps^2)/mum^2]
 tauC    = 1;                        # Photon lifetime                      [ps]
 tauX    = 400;                      # Exciton lifetime                     [ps]
-g       = 1*(0.2/hbar)*10**(-3);      # Exciton-exciton interaction         [meV]
-t0      = 0.1                      # initial impulse time                [ps]
-W       = np.sqrt(0.4)              #                                     [um]
-Tp      = np.sqrt(0.003)            #                                     [ps]
-F0      = 200
-from tqdm import tqdm
-def second_deriv_diff(fun):
-  return (np.roll(fun,1)+np.roll(fun,-1)-2*fun)/(dx**2)
-def second_deriv_k(fun):
+g       = 1*(0.2/hbar)*10**(-3);    # Exciton-exciton interaction         [meV]
+t0      = 0.1;                      # initial impulse time                [ps]
+W       = np.sqrt(0.4);             # gaussian variance of signal         [um]
+Tp      = np.sqrt(0.003);           # impulse duration                    [ps]
+F0      = 200;                      # pump amplitude               
+
+-----------------------------------------------------------------------------#
+def second_deriv_diff(fun):                                              # 2D second derivative (laplace) operator implementation in the finite difference method 
+  return (np.roll(fun,1)+np.roll(fun,-1)-2*fun)/(dx**2)   
+
+
+def second_deriv_k(fun):                                                  # 2D second derivative (laplace) operator implementation in the pseudospectral method
   k = np.concatenate((np.array(np.arange(0,Nx/2,1)*2*np.pi/Lx),np.array(np.arange(-Nx/2,0,1)*2*np.pi/Lx)))
   k2 = k*k
   return np.fft.ifft(-k2*np.fft.fft(fun))
-def pump(x,t):
+
+
+def pump(x,t):                                                             # Gaussian pump in space and time representing the laser pulse that excites the system
   return F0*np.exp(-(x**2)/(W**2) - (t/Tp)**2 - (1j/hbar)*0*(t-t0))
-x = np.arange(-Lx/2,Lx/2,dx)
-psi_x = 0.0025*np.exp(-x**2)
-psi_c = x*0
+
+
+x = np.arange(-Lx/2,Lx/2,dx)        
+psi_x = 0.0025*np.exp(-x**2)      #initial state of the excitonic component of exciton-polariotons
+psi_c = x*0                       #initial state of the photonic component of exciton-polariotns
 time = np.arange(t0,tmax,dt)
 psi_x_data = np.zeros([len(time),Nx],dtype = 'complex')
 psi_c_data = np.zeros([len(time),Nx],dtype = 'complex')
@@ -65,6 +72,7 @@ F_data = np.zeros([len(time),Nx],dtype = 'complex')
 psiC_data_ks = np.zeros([len(time),Nx],dtype = 'complex')
 xm,tm = np.meshgrid(x,time)
 t = t0
+#---------------------------------- EVOLUTION OF THE SYSTEM CALCULATED USING THE 4TH ORDER RUNGE-KUTTA ALGORITHM------------------------#
 for i in tqdm(range(len(time))):
   psi_x_data[i,:] = psi_x
   psi_c_data[i,:] = psi_c
